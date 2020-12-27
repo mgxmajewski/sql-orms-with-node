f60e066d1560d6c4f20d879468a1606c74d0be3d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-27 17:54:29 +0100

    Adds custom validators to 'runtime' and 'releaseDate' columns

--- db/models/movie.js
+++ db/models/movie.js
@@ -26,7 +26,11 @@ module.exports = (sequelize) => {
             validate: {
                 notNull: {
                     msg: 'Please provide a value for "runtime"',
-                }
+                },
+                min: {
+                    args: 1,
+                    msg: 'Please provide a value greater than "0" for "runtime"'
+                },
             },
         },
         releaseDate: {
@@ -35,7 +39,11 @@ module.exports = (sequelize) => {
             validate: {
                 notNull: {
                     msg: 'Please provide a value for "releaseDate"',
-                }
+                },
+                isAfter: {
+                    args: '1895-12-27',
+                    msg: 'Please provide a value on or after "1895-12-28" for "releaseDate"',
+                },
             },
         },
         isAvailableOnVHS: {
