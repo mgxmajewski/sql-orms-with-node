d473f1b125f6fe7c58e7a288720d093f681c23a3
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-27 17:46:56 +0100

    Adds validation to movie.js model

--- db/models/movie.js
+++ db/models/movie.js
@@ -11,19 +11,38 @@ module.exports = (sequelize) => {
         title: {
             type: Sequelize.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value fot "title"',
+                },
+                notEmpty: {
+                    msg: 'Please provide a value fot "title"',
+                },
+            },
         },
         runtime: {
             type: Sequelize.INTEGER,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "runtime"',
+                }
+            },
         },
         releaseDate: {
             type: Sequelize.DATEONLY,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "releaseDate"',
+                }
+            },
         },
         isAvailableOnVHS: {
             type: Sequelize.BOOLEAN,
             allowNull: false, // disallow null
-            defaultValue: false
+            defaultValue: false,
+            validate: { },
         }
     }, { sequelize });
 
