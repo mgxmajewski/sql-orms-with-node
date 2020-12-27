16de34e34a8aaf763bfdce9edfb734fc162dd3e2
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-27 02:18:04 +0100

    Synchronize Models with the Database

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const sequelize = new Sequelize({
   storage: 'movies.db',
 });
 
+// Movie model
+class Movie extends Sequelize.Model {}
+Movie.init({
+  title: Sequelize.STRING
+}, { sequelize });
+
+(async () => {
+  // Sync 'Movies' table
+  await sequelize.sync({ force: true });
+
+  try {
+
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
+
 (async () => {
   try {
     await sequelize.authenticate();
