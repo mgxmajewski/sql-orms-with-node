4cd73fba4aba176959135065d014c43a0c68737a
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-27 02:48:44 +0100

    Refactors app.js to use new project layout

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'movies.db',
-});
-
-// Movie model
-class Movie extends Sequelize.Model {}
-Movie.init({
-  title: Sequelize.STRING
-}, { sequelize });
+const db = require('./db');
+const { Movie } = db.models;
 
 (async () => {
-  // Sync 'Movies' table
-  await sequelize.sync({ force: true });
+  await db.sequelize.sync({ force: true });
 
   try {
-    const movieInstances = await Promise.all([
-      Movie.create({
-        title: 'Toy Story'
-      }),
-      Movie.create({
-        title: 'The Incredibles'
-      }),
-    ]);
-    const moviesJSON = movieInstances.map(movie => movie.toJSON());
-    console.log(moviesJSON);
+    const movie = await Movie.create({
+      title: 'Toy Story'
+    });
+    console.log(movie.toJSON());
+
+    const movie2 = await Movie.create({
+      title: 'The Incredibles'
+    });
+    console.log(movie2.toJSON());
 
   } catch (error) {
     console.error('Error connecting to the database: ', error);
   }
 })();
-
-
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connection to the database successful!');
-//   } catch (error) {
-//     console.error('Error connecting to the database: ', error);
-//   }
-// })();
